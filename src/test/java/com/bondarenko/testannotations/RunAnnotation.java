b6067944c6e3db9270e9cf82422841cc1e78fe81
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-04-28 21:05:54 +0300

    add RunAnnotation;

--- src/test/java/com/bondarenko/testannotations/RunAnnotation.java
+++ src/test/java/com/bondarenko/testannotations/RunAnnotation.java
@@ -5,15 +5,15 @@ import com.bondarenko.annotations.Run;
 public class RunAnnotation {
     @Run
     public void method1() {
+        System.out.println("run");
     }
 
     @Run
     public void method2() {
+        System.out.println("call");
     }
 
     public void method3() {
-    }
-
-    public void method4() {
+        System.out.println("delete");
     }
 }
