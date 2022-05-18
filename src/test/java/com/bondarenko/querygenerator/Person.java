a8b9329eb4b0302967c9b3cdfd8c75969ee2d0f4
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-05-18 13:31:10 +0300

    add Person

--- src/test/java/com/bondarenko/querygenerator/Person.java
+++ src/test/java/com/bondarenko/querygenerator/Person.java
@@ -2,6 +2,7 @@ package com.bondarenko.querygenerator;
 
 @Table
 public class Person {
+    @Key
     @Column(name = "person_id")
     private int id;
 
@@ -10,4 +11,10 @@ public class Person {
 
     @Column(name = "person_salary")
     private double salary;
+
+    public Person(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
 }
