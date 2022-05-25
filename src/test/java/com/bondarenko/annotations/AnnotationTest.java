a77c97b04b0a870f3f949a8fc8b78e2f969d3a2c
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-05-26 01:53:21 +0300

    add AnnotationTest

---
+++ src/test/java/com/bondarenko/annotations/AnnotationTest.java
@@ -0,0 +1,39 @@
+package com.bondarenko.annotations;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AnnotationTest {
+
+    private AnnotationUtils annotationUtils = new AnnotationUtils();
+    private RunAnnotation runAnnotation = new RunAnnotation();
+    private InjectAnnotation injectAnnotation = new InjectAnnotation();
+
+    @Test
+    @DisplayName("test Invoke Methods With Annotation Run")
+    public void testInvokeMethods_WithAnnotationRun() {
+        List<String> methodsWithAnnotationRun = annotationUtils.invokeMethodsWithAnnotationRun(RunAnnotation.class, runAnnotation);
+
+        assertTrue(methodsWithAnnotationRun.contains("method1"));
+        assertTrue(methodsWithAnnotationRun.contains("method2"));
+        assertFalse(methodsWithAnnotationRun.contains("method3"));
+
+        assertEquals(2, methodsWithAnnotationRun.size());
+    }
+
+    @Test
+    @DisplayName("test Fill Fields With Annotation Inject By Object Of Annotation Clazz")
+    public void testFillFields_WithAnnotationInject_ByObject_OfAnnotationClazz() {
+        List<String> fieldsWithAnnotationInject = annotationUtils.fillFieldsWithAnnotationInjectByObjectOfAnnotationClazz(injectAnnotation);
+
+        assertTrue(fieldsWithAnnotationInject.contains("name"));
+        assertTrue(fieldsWithAnnotationInject.contains("list1"));
+        assertTrue(fieldsWithAnnotationInject.contains("list2"));
+
+        assertEquals(3, fieldsWithAnnotationInject.size());
+    }
+}
